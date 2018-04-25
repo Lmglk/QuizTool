de1ecfcea03b4022c05155b38259279ce99c77a7
Leonid Veretennikov <megalok10@gmail.com>
2018-04-26 02:58:08 +0300

    Almost implemented check of answers

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,32 +1,87 @@
 import * as React from "react";
 import {Component, ReactNode} from "react";
 
-import {Quest} from "../Quest/Quest";
+import Quest from "../Quest/Quest";
 
 import "./Content.sass";
 
-import {data} from "./data";
+import {data, answers} from "./data";
 
-export class Content extends Component {
-    private questions: Array<ReactNode> = [];
+export interface IOption {
+    title: string,
+    value: boolean
+}
+
+interface IQuestion {
+    id: number;
+    title: string;
+    options: Array<IOption>;
+}
+
+interface IContentState {
+    questions: Array<IQuestion>;
+}
+
+export class Content extends Component<any, IContentState> {
+    private questions: Array<IQuestion>;
 
     constructor(props: any) {
         super(props);
 
-        data.forEach(question => {
-            this.questions.push(
-                <Quest question={question.question} options={question.options} key={`quest-${question.id}`}/>
-            );
+        this.questions = [];
+        data.forEach(quest => {
+            let options: Array<IOption> = [];
+            quest.options.forEach(option => {
+                options.push({
+                  title: option,
+                  value: false
+                });
+            });
+            this.questions.push({
+                id: quest.id,
+                title: quest.title,
+                options: options
+            });
+
+            this.state = {
+                questions: this.questions
+            }
+        });
+    }
+
+    private turnAnswer(index: number, event: any): void {
+        const newState: Array<IQuestion> = this.state.questions;
+        let option: IOption = newState[index].options.find(option => option.title === event.target.value);
+        option.value = event.target.checked;
+        this.setState({
+            questions: newState
+        });
+    }
+
+    private checkAnswer(): void {
+        this.state.questions.forEach((quest, index) => {
+            let right = true;
+            quest.options.forEach(option => {
+                answers[index].answer.forEach(answer => {
+                    if ((answer === option.title && !option.value) || (answer !== option.title && option.value))
+                        right = false;
+                })
+            });
+            console.log(quest.title, " - ", right);
         });
     }
 
     render(): ReactNode {
+        let questionsNode: Array<React.ReactNode> =
+            this.questions.map((quest, index) =>
+                <Quest key={quest.id} question={quest} turnAnswer={this.turnAnswer.bind(this, index)}/>
+            );
+
         return (
             <div className="container app-body">
                 <div className="row">
-                    <div className="col-12">
-                        {this.questions}
-                    </div>
+                    <button className="btn btn-default" onClick={this.checkAnswer.bind(this)}>Проверить</button>
+                    <div className="col-12">{questionsNode}</div>
                 </div>
             </div>
         );
--- src/components/Quest/Quest.tsx
+++ src/components/Quest/Quest.tsx
@@ -1,42 +1,26 @@
 import * as React from "react";
-import {Component, ReactNode} from "react";
+import {ReactNode} from "react";
 
 import "./Quest.sass";
+import {IOption} from "../Content/Content";
 
-export interface IQuest {
-    question: string
-    options: Array<string>,
-}
-
-export class Quest extends Component<IQuest> {
-    private options: Array<Object> = [];
-    private question: any;
-
-
-    constructor(props: IQuest) {
-        super(props);
-
-        this.question = props.question;
-
-        props.options.forEach((option: any) => {
-            this.options.push(
-                <div className="quest-option" key={`option-for-quest-${option}`}>
-                    <input type="checkbox" key={`input-${option}`}
-                           value={option}/>
-                    <label key={`label-${option}`}>{option}</label>
+function Quest(props: any) {
+    let options: Array<ReactNode> = props.question.options
+        .map((option: IOption, index: number) => {
+            return (
+                <div className="quest-option" key={`option-${index}`}>
+                    <input type="checkbox" value={option.title} key={`input-${index}`} onChange={props.turnAnswer}/>
+                    <label key={`label-${index}`}>{option.title}</label>
                 </div>
             );
         });
-    }
 
-    render(): ReactNode {
-        return (
-            <div className="quest">
-                <div className="quest-title">{this.question}</div>
-                <div className="quest-options">
-                    {this.options}
-                </div>
-            </div>
-        );
-    }
+    return (
+        <div className="quest">
+            <div className="quest-title">{props.question.title}</div>
+            <div className="quest-options">{options}</div>
+        </div>
+    );
 }
+
+export default Quest;
